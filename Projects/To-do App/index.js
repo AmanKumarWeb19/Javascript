6b69d37f2267d5f7181509abddb4312d4fa9562c
AmanKumarWeb19 <akgupta0524@gmail.com>
2023-12-11 15:41:47 +0530

    add date

--- Projects/To-do App/index.js
+++ Projects/To-do App/index.js
@@ -1,11 +1,23 @@
-let todoList = ["Buy Milk", "Go to College"];
+let todoList = [
+  {
+    item: "Buy Milk",
+    dueDate: 11 / 12 / 2023,
+  },
+  {
+    item: "Go to College",
+    dueDate: 11 / 12 / 2023,
+  },
+];
 displayItem();
 
 function addTodo() {
   let inputElement = document.querySelector("#todoInput");
+  let inputDate = document.querySelector("#todo-date");
   let todoItem = inputElement.value;
-  todoList.push(todoItem);
+  let todoDate = inputDate.value;
+  todoList.push({ item: todoItem, dueDate: todoDate });
   inputElement.value = "";
+  inputDate.value = "";
   displayItem();
 }
 
@@ -15,9 +27,11 @@ function displayItem() {
   let newHtml = "";
 
   for (i = 0; i < todoList.length; i++) {
+    let { item, dueDate } = todoList[i];
     newHtml += `
     <div>
-    <span>${todoList[i]}</span>
+    <span>${item}</span>
+    <span>${dueDate}</span>
     <button onclick='todoList.splice(${i},1); displayItem();'>Delete</button>
     </div>
    
